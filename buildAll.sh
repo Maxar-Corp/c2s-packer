5f3797ea7da8e36bf53749686e2b1c50bc03ac4e
mkrutz <mkrutz@DigitalGlobe.com>
2018-04-25 13:01:59 -0400

    Fixed build issues

--- buildAll.sh
+++ buildAll.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+basepath=`pwd`
+
 cd src/github.com/kardianos/govendor
 go build
+cp ./govendor ../../../../bin
 cd ../../mitchellh/gox
 go build
 cd ../../hashicorp/packer
 make standalone
+cp ${basepath}/src/github.com/hashicorp/packer/pkg/linux_amd64/packer ${basepath}/bin
